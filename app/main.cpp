767f0ad34c02e84ea83e0ae83de22afa1555f1fc
Vitor <vitorclcoelho@gmail.com>
2019-12-17 18:02:21 -0300

    Add SDL
    Statically add lib and a lib loader config

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
-
-using namespace std;
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 
 int main(int argc, char const *argv[])
 {
-    cout<<"Hello World"<<endl;
+    SDL_Window *window;
+    
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window = SDL_CreateWindow(
+        "An SDL2 window",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_OPENGL
+    );
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
     return 0;
 }
